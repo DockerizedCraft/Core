7e176869968b00fdd4b4c634f0d57edf2eed8eea
Sven Mawby <fr34kyn01535@bam.yt>
2019-01-25 00:10:40 +0100

    Initial implementation

--- src/main/java/de/craftmania/dockerizedcraft/container/inspector/ContainerInspector.java
+++ src/main/java/de/craftmania/dockerizedcraft/container/inspector/ContainerInspector.java
@@ -1,64 +1,28 @@
 package de.craftmania.dockerizedcraft.container.inspector;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
 
-import com.github.dockerjava.api.DockerClient;
-import com.github.dockerjava.api.model.Container;
-import com.github.dockerjava.api.model.Event;
-import com.github.dockerjava.api.model.EventType;
-import com.github.dockerjava.core.command.EventsResultCallback;
-import de.craftmania.dockerizedcraft.container.inspector.client.DockerClientFactory;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.config.Configuration;
 
-import java.io.IOException;
-import java.util.List;
 import java.util.logging.Logger;
 
 public class ContainerInspector {
-    private DockerClient dockerClient;
-
     private ProxyServer proxyServer;
-
-    private String network;
-
     private Logger logger;
 
     public ContainerInspector(Configuration configuration, ProxyServer proxyServer, Logger logger) {
         this.proxyServer = proxyServer;
-        this.network = configuration.getString("network");
         this.logger = logger;
-        this.dockerClient = DockerClientFactory.getByConfiguration(configuration);
     }
 
     public void runContainerInspection() {
-        this.logger.info("[Container Inspector] Running initial inspection.");
-
-        EventsResultCallback callback = this.getEventResultCallback();
-        List<Container> containers = this.dockerClient.listContainersCmd().exec();
-
-        // Trigger fake Event to use same Result Callback
-        for (Container container: containers) {
-            Event event = new Event("start", container.getId(), container.getImage(), System.currentTimeMillis())
-                    .withAction("bootstrap")
-                    .withType(EventType.forValue("container"));
-
-            callback.onNext(event);
-        }
+        //Not running initial inspection, listener does that
     }
 
     public void runContainerListener() {
         this.logger.info("[Container Inspector] Running listener.");
-        try {
-            this.dockerClient.eventsCmd().exec(this.getEventResultCallback()).awaitCompletion().close();
-        } catch (IOException |InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private EventsResultCallback getEventResultCallback() {
-        return new ResultCallback(
-                this.dockerClient,
-                this.proxyServer,
-                this.network
-        );
+        KubernetesClient client = new DefaultKubernetesClient();
+        client.pods().inNamespace("che").withLabel("dockerizedcraft/enabled=true").watch(new PodWatcher(proxyServer, logger));
     }
 }
---
+++ src/main/java/de/craftmania/dockerizedcraft/container/inspector/PodWatcher.java
@@ -0,0 +1,49 @@
+package de.craftmania.dockerizedcraft.container.inspector;
+import net.md_5.bungee.api.ProxyServer;
+import io.fabric8.kubernetes.api.model.Pod;
+import io.fabric8.kubernetes.client.Watcher;
+import de.craftmania.dockerizedcraft.container.inspector.events.ContainerEvent;
+import io.fabric8.kubernetes.client.KubernetesClientException;
+import io.fabric8.kubernetes.api.model.EnvVar;
+import java.util.logging.Logger;
+import java.net.InetAddress;
+import java.util.*;
+
+public class PodWatcher implements Watcher<Pod> {
+
+    private ProxyServer proxyServer;
+    private Logger logger;
+    public PodWatcher(ProxyServer proxyServer, Logger logger) {
+        this.proxyServer = proxyServer;
+        this.logger = logger;
+    }
+
+    @Override
+    public void eventReceived(Action action, Pod resource) {
+        logger.info(action.name());
+        logger.info(resource.getStatus().getPhase());
+        logger.info(resource.getStatus().getPodIP());
+        logger.info(resource.getMetadata().getName());
+        logger.info(resource.getSpec().getContainers().get(0).getEnv().toString());
+        logger.info(resource.getMetadata().getLabels().toString());
+
+        try {
+            ContainerEvent containerEvent = new ContainerEvent(resource.getMetadata().getName(), action.name());
+            containerEvent.setName(resource.getMetadata().getName());
+            Map<String ,String > environmentVariables = new HashMap<>();
+            for (EnvVar i : resource.getSpec().getContainers().get(0).getEnv()) environmentVariables.put(i.getName(),i.getValue());
+            containerEvent.setEnvironmentVariables(environmentVariables);
+            containerEvent.setPort(Integer.parseInt(environmentVariables.get("SERVER_PORT")));
+            containerEvent.setIp(InetAddress.getByName(resource.getStatus().getPodIP()));
+            this.proxyServer.getPluginManager().callEvent(containerEvent);
+        }catch(java.net.UnknownHostException ex){
+            logger.severe(ex.getMessage());
+        }
+
+    }
+
+    @Override
+    public void onClose(KubernetesClientException cause) {
+        logger.warning("Watcher close due to " + cause);
+    }
+}
